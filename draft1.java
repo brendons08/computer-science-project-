48c16dd0c27c9c3d4c347f08620c7a2f921374ac
brendons08 <440034605@gapps.yrdsb.ca>
2025-06-14 00:15:27 -0400

    Update draft1.java

--- draft1.java
+++ draft1.java
@@ -17,7 +17,25 @@ public class main {
             System.out.print("you choice to add a service! now enter service name: ");
              String servicename = input.nextLine();
               System.out.println("your services name is " + "\"" + servicename + "\"");
+         } else if (choice == 2) {
+            ArrayList<String> categories = new ArrayList<>();
+            categories.add("Plumbing");
+            categories.add("Tutoring");
+            categories.add("Cleaning");
+                  System.out.println("you chose to book a service!Available service categories:");
+                     int number = 1;
+                     for (String category : categories) {
+                    System.out.println(number + ". " + category);
+                     number++;
+                     }
+                     
+                      System.out.print("Enter the number of the category you want to view: ");
+            
+
+
+              
+         }  
          }
     }
-}
+
   
